68d9f97e4645f92589146baa6f7d5b3d86c96cae
nague <nagueravanely@gmail.com>
2019-07-05 07:35:55 +0100

    Adding JPA entities

---
+++ src/main/java/nsi/spring/springguru/SpringguruApplication.java
@@ -0,0 +1,13 @@
+package nsi.spring.springguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringguruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringguruApplication.class, args);
+    }
+
+}
